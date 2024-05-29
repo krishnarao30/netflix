a7d667f4ad6d91ed3de32ab09a429585c1ab891f
krishnarao30 <93366369+krishnarao30@users.noreply.github.com>
2024-05-29 08:21:13 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import Home from "./pages/home/Home";
+
+import Register from "./pages/register/Register";
+import Watch from "./pages/watch/Watch";
+import Login from "./pages/login/Login";
+
+const App = () => {
+  return (
+    <Login/>
+  )
+};
+
+export default App;
